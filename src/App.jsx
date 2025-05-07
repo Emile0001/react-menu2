0c0778e5315ee4c00dd26f57fbc10ecdfe3fef1a
Emile0001 <boteseh@gmail.com>
2025-05-07 10:32:37 +0200

    one liner - unique categories

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,16 @@ import menu from "./data";
 import Title from "./components/Title/Title";
 import Menu from "./components/Menu/Menu";
 
+// const tempCategories = menu.map((item) => item.category);
+// const tempSet = new Set(tempCategories);
+// const allCategories = ["all", ...tempSet];
+//one liner
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
 function App() {
     const [menuItems, setMenuItems] = useState(menu);
+    const [categories, setCategories] = useState(allCategories);
+    console.log(categories);
 
     return (
         <main>
